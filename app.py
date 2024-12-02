f2ea7bec80785581f56acc1e2fcb6e9d476ee68d
JuliaFischer-code <jufischer59@gmail.com>
2024-12-02 13:44:56 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/call")
+def call():
+    return render_template("call.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+@app.route("/sos")
+def sos():
+    return render_template("sos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
